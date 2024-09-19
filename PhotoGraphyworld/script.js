dfe64c2f2caef59ec4a70ad77ba11fb257936fb2
mrudulasadafule03 <153377630+mrudulasadafule03@users.noreply.github.com>
2024-09-19 20:14:53 +0530

    Add files via upload

---
+++ PhotoGraphyworld/script.js
@@ -0,0 +1,27 @@
+function s1(){
+    window.location.href = "newyork.html";
+}
+function s2(){
+    window.location.href = "singapore.html";
+}
+function s3(){
+    window.location.href = "switzerland.html";
+}
+function s4(){
+    window.location.href = "srilanka.html";
+}
+function s5(){
+    window.location.href = "kenya.html";
+}
+function s6(){
+    window.location.href = "kanha.html";
+}
+function s7(){
+    window.location.href = "dudhsagar.html";
+}
+function s8(){
+    window.location.href = "northlight.html";
+}
+function s9(){
+    window.location.href = "china.html";
+}
